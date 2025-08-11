2bf6764cd311dff04663f94bfd4552cdcdcf590a
Darieth <darieth.s.v@gmail.com>
2025-08-11 18:10:11 -0500

    Starting to do a new project

---
+++ laravelcourse/routes/web.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', 'App\Http\Controllers\HomeController@index')->name("home.index");
+Route::get('/about', function () {
+
+$data1 = "About us - Online Store";
+
+$data2 = "About us";
+
+$description = "This is an about page ...";
+
+$author = "Developed by: Your Name";
+
+return view('home.about')->with("title", $data1)
+
+->with("subtitle", $data2)
+
+->with("description", $description)
+
+->with("author", $author);
+
+})->name("home.about");
+
+Route::get('/contact', function () {
+
+$data1 = "Contact us - Online Store";
+
+$data2 = "Contact us";
+
+$email = "darieth@prueba.com";
+
+$address = "address: Calle 1 #2-3";
+
+$phone_number="123456789";
+
+return view('home.contact')->with("title", $data1)
+
+->with("subtitle", $data2)
+
+->with("email", $email)
+
+->with("address", $address)
+
+->with("phone_number", $phone_number);
+
+})->name("home.contact");
